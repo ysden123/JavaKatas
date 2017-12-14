a76dbb62b8fd0b93fdc219f3b7d8dd2b9ca9d70a
Yuriy Stul <Yuriy.Stul@WilliamHill.com>
2017-12-14 15:27:00 +0200

    last version of Cucumber

--- NumberGames/src/test/java/com/example/CukesRunner.java
+++ NumberGames/src/test/java/com/example/CukesRunner.java
@@ -1,10 +1,12 @@
 package com.example;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(
-        features={"src/test/resources"}
+@CucumberOptions(
+        features = {"src/test/resources"}
 )
-public class CukesRunner {}
+public class CukesRunner {
+}
